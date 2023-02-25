38e4fe7f0cdc76ef882b48a809c194800fcd4755
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 15:20:43 +0300

    connected the MainContent.jsx component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div className='container'>
       <Header />
+      <MainContent />
     </div>
   );
 }
