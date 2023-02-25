f7ad6fe949134dbac06fb8f923239f1a8ad2660a
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 16:29:56 +0300

    added new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='container'>
       <Header />
       <MainContent />
+      <Footer />
     </div>
   );
 }
