a1e3573bf56a681db6cfcc734cd94048365c8941
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 15:56:19 +0300

    update classes for buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,13 +3,13 @@ import refresh from "../images/refresh.png";
 function Buttons(){
     return(
         <div className="buttons">
-            <button className="button-1">
+            <button className="button-plus counter">
                 +
             </button>
-            <button className="button-2">
+            <button className="button-refresh counter">
                 <img src={refresh} alt="" />
             </button>
-            <button className="button-3">
+            <button className="button-minus counter">
                 -
             </button>
         </div>
