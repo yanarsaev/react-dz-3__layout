df582a96e1f0a55721bd472632a443d6e4a051c4
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 13:34:59 +0300

    create Header.jsx component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "./Menu";
+
+function Header(){
+    return(
+        <div className="header">
+            <Menu/>
+        </div>
+    )
+}
+
+export default Header;
