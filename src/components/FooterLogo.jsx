96682cdb920700613b08f86cbe2958dd5d2243d0
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 16:28:13 +0300

    update FooterLogo.jsx component

---
+++ src/components/FooterLogo.jsx
@@ -0,0 +1,11 @@
+import footer from '../images/footer.png';
+
+function FooterLogo(){
+    return(
+        <div className="footer-logo">
+            <a href="#"><img src={footer} alt="" /></a>
+        </div>
+    )
+}
+
+export default FooterLogo;
