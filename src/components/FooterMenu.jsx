46089ebb811edccedc50c0d004c88e114510d84d
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 16:27:49 +0300

    create FooterMenu.jsx component

---
+++ src/components/FooterMenu.jsx
@@ -0,0 +1,17 @@
+function FooterMenu(){
+    return(
+        <div className="footer-menu">
+        <a href="#">
+            <p>Партнерам</p>
+        </a>
+        <a href="#">
+            <p>Разработчикам</p>
+        </a>
+        <a href="#">
+            <p>Вакансии</p>
+        </a>
+    </div>
+    )
+}
+
+export default FooterMenu;
