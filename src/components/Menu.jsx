617dc93ae9cbf9815465ef68ef7cfae92979ba7d
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 13:35:19 +0300

    create Menu.jsx component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Menu(){
+    return(
+        <nav>
+            <ul>
+                <li>Главная</li>
+                <li>О нас</li>
+                <li>Контакты</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu;
