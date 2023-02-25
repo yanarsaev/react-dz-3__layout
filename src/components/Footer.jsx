bf6388a0c9d6581b9db3b84ddb013009315c44b6
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-25 16:00:45 +0300

    create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import FooterLogo from './FooterLogo';
+
+function Footer(){
+    return(
+        <div className='Footer'>
+            <FooterLogo />
+        </div>
+    )
+}
+
+export default Footer;
