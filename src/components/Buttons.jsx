50157135ee166bbd6f55c67f9cb271a60ec0b338
Yanarsaev Magomed <yanarsaevit@internet.ru>
2023-02-26 16:37:30 +0300

    added new functions for buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,15 +1,29 @@
 import refresh from "../images/refresh.png";
 
-function Buttons(){
+function Buttons({counter, SetCounter}){
+    function plus(){
+        SetCounter(counter + 3)
+    }
+    function minus(){
+        SetCounter(counter - 2)
+        if (counter===0){
+            return SetCounter(0);
+        }
+    }
+    function reset(){
+        SetCounter(0)
+    }
+
+
     return(
         <div className="buttons">
-            <button className="button-plus counter">
+            <button className="button-plus counter" onClick={plus}>
                 +
             </button>
-            <button className="button-refresh counter">
+            <button className="button-refresh counter" onClick={reset}>
                 <img src={refresh} alt="" />
             </button>
-            <button className="button-minus counter">
+            <button className="button-minus counter" onClick={minus}>
                 -
             </button>
         </div>
